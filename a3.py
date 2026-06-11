4e683dbd25c6b41cec43c8f7b1728b8ad43728ed
Hridayesh Malik <malikhridayesh@gmail.com>
2026-06-11 20:42:18 +0530

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,24 @@
+def add(p,q):
+    return p+q
+def subtract(p,q):
+    return p-q
+def multiply(p,q):
+    return p*q
+def divide(p,q):
+    return p/q
+print("please select the operation you want to perform:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+choice = input("Enter choice(1/2/3/4):")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if choice == '1':
+    print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+    print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+    print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '4':
+    print(num1,"/",num2,"=", divide(num1,num2))
